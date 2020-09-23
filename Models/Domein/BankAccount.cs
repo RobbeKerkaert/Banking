c9c6cbd1c110d5bcedeebfae465e07f944344844
Robbe Kerkaert <robbe.kerkaert@student.hogent.be>
2020-09-23 10:08:24 +0200

    yes

--- Models/Domein/BankAccount.cs
+++ Models/Domein/BankAccount.cs
@@ -1,16 +1,23 @@
-﻿namespace Banking.Models.Domein
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Banking.Models.Domein
 {
     public class BankAccount
     {
+        private readonly System.Collections.IList _transactions;
         #region Properties
         public decimal Balance { get; private set; } = 0M;
         public string AccountNumber { get; }
+        public int NumberOfTransactions { get { return _transactions.Count; } }
         #endregion
 
         #region Constructor
         public BankAccount(string accountNumber)
         {
             AccountNumber = accountNumber;
+            _transactions = new List<Transaction>();
         }
         #endregion
 
@@ -18,11 +25,18 @@
         public void Deposit(decimal amount)
         {
             Balance += amount;
+            _transactions.Add(new Transaction(amount, TransactionType.Deposit));
         }
 
         public void Withdraw(decimal amount)
         {
             Balance -= amount;
+            _transactions.Add(new Transaction(amount, TransactionType.Withdraw));
+        }
+
+        public IEnumerable<Transaction> GetTransactions(DateTime? from, DateTime? till)
+        {
+            throw new NotImplementedException();
         }
         #endregion
     }
---
+++ Models/Domein/Transaction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Banking.Models.Domein
+{
+    public class Transaction
+    {
+        public decimal Amount { get; }
+        public DateTime DateOfTrans { get; }
+        public TransactionType TransactionType { get; }
+        public bool isDeposit { get { return TransactionType == TransactionType.Deposit; } }
+
+        public Transaction(decimal amount, TransactionType type)
+        {
+            Amount = amount;
+            TransactionType = type;
+            DateOfTrans = DateTime.Now;
+        }
+    }
+}
